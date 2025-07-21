607e210e9827c288dfeaf1dc2a4162f02c146feb
Jason Kao <jason@fogsecurity.io>
2025-07-21 16:29:32 -0400

    policy to dict

--- modules/key_policy_check.py
+++ modules/key_policy_check.py
@@ -1,18 +1,20 @@
 import sys
 import boto3
+import json
 
 #Module processes KMS Key Policies to check for potential cross-account usage.
 
-def parse_policy(session, key_region, input_key_arn):
+
+def get_key_policy(session, key_region, input_key_arn):
     try:
         kms_client = session.client('kms', region_name=key_region)
         key_policy = kms_client.get_key_policy(
             KeyId = input_key_arn
         )
-        print(key_policy)
+        return key_policy
     except:
         sys.exit("Error getting key policy")
 
-
-
-#def check_policy
+def parse_policy(raw_key_policy):
+    key_policy = json.loads(raw_key_policy['Policy'])
+    return key_policy                    
