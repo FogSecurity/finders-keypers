e2b9e320180ce237d5401e36856a624b78852aa1
Jason Kao <jason@fogsecurity.io>
2025-07-21 16:04:49 -0400

    initial key policy pull

--- modules/key_policy_check.py
+++ modules/key_policy_check.py
@@ -1,1 +1,18 @@
+import sys
+import boto3
 
+#Module processes KMS Key Policies to check for potential cross-account usage.
+
+def parse_policy(session, key_region, input_key_arn):
+    try:
+        kms_client = session.client('kms', region_name=key_region)
+        key_policy = kms_client.get_key_policy(
+            KeyId = input_key_arn
+        )
+        print(key_policy)
+    except:
+        sys.exit("Error getting key policy")
+
+
+
+#def check_policy
